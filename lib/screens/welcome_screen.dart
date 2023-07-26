16e5af0857cdf780669a11f7a08788207d06bd29
Denis Skerk <g3rard@Deniss-Air.lan>
2023-07-26 18:57:10 +0200

    Refactored Buttons, added dynamic text

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
@@ -9,11 +10,87 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class MyRoundedButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final VoidCallback onPressed;
+
+  const MyRoundedButton({
+    this.text,
+    this.color,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: () {
+            Navigator.pushNamed(context, LoginScreen.id);
+          },
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            text,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+
+  AnimationController controller; // Controller
+  Animation animation; // Used for different curve animations
+
+  @override void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 3),
+      vsync: this, // Ticker
+      //upperBound: 100.0,
+    );
+
+    //animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+    //controller.reverse(from: 1.0);
+
+    controller.forward();
+    //animation.addStatusListener((status) {
+    //  print(status);
+    //  if (status == AnimationStatus.completed) {
+    //    controller.reverse(from:1.0);
+    //  } else if (status == AnimationStatus.dismissed) {
+    //    controller.forward();
+    //  }
+    //});
+
+    animation = ColorTween(begin: Colors.deepPurple, end: Colors.blueAccent).animate(controller);
+
+    controller.addListener(() {
+      setState(() {
+
+      });
+      print(animation.value);
+    });
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value, //Colors.red.withOpacity(controller.value),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -26,12 +103,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: 60.0, //animation.value*100,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                TyperAnimatedTextKit(
+                  //'${(controller.value).toInt()}%',
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
                   ),
@@ -41,45 +119,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            MyRoundedButton(color: Colors.lightBlueAccent, text: "Log In", onPressed: () { Navigator.pushNamed(context, LoginScreen.id); },),
+            MyRoundedButton(color: Colors.blueAccent, text: "Register", onPressed: () { Navigator.pushNamed(context, RegistrationScreen.id); },),
           ],
         ),
       ),
     );
   }
 }
+
